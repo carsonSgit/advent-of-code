796f75425664e1bffc98713945fdbd3103b05e2d
Carson <92652800+carsonSgit@users.noreply.github.com>
2025-12-06 16:01:56 -0500

    day 5 part 2

---
+++ day5/part2.ts
@@ -0,0 +1,41 @@
+import * as fs from 'fs';
+
+const filePath: string = './day5/input.txt';
+const input: string = fs.readFileSync(filePath, 'utf8');
+
+const [rangeBlock] = input.trim().split('\n\n');
+
+const ranges = rangeBlock
+    .trim()
+    .split('\n')
+    .map(line => line.trim().split('-').map(Number)) as [number, number][];
+
+ranges.sort(
+    (a: [number, number], b: [number, number]) => a[0] - b[0]
+    );
+
+const merged: [number, number][] = [];
+
+for (const [start, end] of ranges) {
+    if (merged.length === 0){
+        merged.push([start, end]);
+        continue;
+    }
+
+    const last = merged[merged.length - 1];
+    if (start <= last[1]) {
+        last[1] = Math.max(last[1], end);
+    } else {
+        merged.push([start, end]);
+    }
+}
+
+
+let totalFresh = 0;
+
+for (const [a, b] of merged) {
+    totalFresh += (b - a + 1);
+}
+
+
+console.log(totalFresh);
